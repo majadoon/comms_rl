545198567c53c1300ab967e6f2142a8c4fa88f62
Muhammad Awais Jadoon <mawaisjadoon@gmail.com>
2020-02-07 22:25:26 +0100

    Add files via upload

---
+++ comms_rl/agents/random_agent.py
@@ -0,0 +1,14 @@
+class RandomAgent:
+    """
+    The world's simplest agent!
+
+    See: https://github.com/openai/gym/blob/master/examples/agents/random_agent.py
+    """
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self, state, reward, done):
+        return self.action_space.sample()
+
+    def seed(self, seed=0):
+        self.action_space.seed(seed)
